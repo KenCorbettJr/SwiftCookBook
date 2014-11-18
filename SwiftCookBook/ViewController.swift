9dd651909e869f563c8e0f7cd0a45611b1c31ce2
Ken Corbett <Kenneth.Corbett@gmail.com>
2014-11-17 20:40:10 -0800

    Initial Commit

---
+++ SwiftCookBook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftCookBook
+//
+//  Created by Corbett, Ken on 11/17/14.
+//  Copyright (c) 2014 Ken Yann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
